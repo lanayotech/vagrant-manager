a1f189cff7ac8fa9d5296860897c3190fb2eff13
Amitai Lanciano <lanciano1@gmail.com>
2014-11-04 19:16:34 -0700

    re-factor/re-implement recursive bookmark scan

--- Vagrant Manager/ManageBookmarksWindow.h
+++ Vagrant Manager/ManageBookmarksWindow.h
@@ -13,11 +13,18 @@
 
 @property (weak) IBOutlet NSButton *recursiveScanCheckbox;
 @property (weak) IBOutlet NSTableView *bookmarkTableView;
+@property (weak) IBOutlet NSButton *cancelScanButton;
+@property (weak) IBOutlet NSTextField *directoryLabelTextField;
+@property (weak) IBOutlet NSButton *addBookmarksButton;
+@property (weak) IBOutlet NSButton *removeBookmarksButton;
+@property (weak) IBOutlet NSButton *cancelButton;
+@property (weak) IBOutlet NSButton *saveButton;
 
 - (IBAction)cancelButtonClicked:(id)sender;
 - (IBAction)saveButtonClicked:(id)sender;
 - (IBAction)addBookmarksButtonClicked:(id)sender;
 - (IBAction)removeBookmarksButtonClicked:(id)sender;
 - (IBAction)recursiveScanCheckboxClicked:(id)sender;
+- (IBAction)cancelScanButtonClicked:(id)sender;
 
 @end
