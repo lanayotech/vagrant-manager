bd1853afd57011281a7db5a036be10c12d975d24
Chris Ayoub <chriswayoub@gmail.com>
2014-11-09 01:54:46 -0700

    add preference to use instance path as the display name

--- Vagrant Manager/NativeMenuItem.m
+++ Vagrant Manager/NativeMenuItem.m
@@ -202,7 +202,12 @@
         if(bookmark) {
             self.menuItem.title = [NSString stringWithFormat:@"[B] %@", bookmark.displayName];
         } else {
-            NSString *title = self.instance.displayName;
+            NSString *title;
+            if([[NSUserDefaults standardUserDefaults] boolForKey:@"usePathAsInstanceDisplayName"]) {
+                title = self.instance.path;
+            } else {
+                title = self.instance.displayName;                
+            }
             
             if(self.instance.machines.count > 0 && [[NSUserDefaults standardUserDefaults] boolForKey:@"includeMachineNamesInMenu"]) {
                 NSMutableArray *machineNames = [[NSMutableArray alloc] init];
